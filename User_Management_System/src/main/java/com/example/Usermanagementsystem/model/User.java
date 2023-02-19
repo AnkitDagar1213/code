ba42008e190a0d59440174045b66256f3d5c1fcb
Dagarshab <112771048+Dagarshab@users.noreply.github.com>
2023-02-19 23:46:32 +0530

    Update User.java

--- User_Management_System/src/main/java/com/example/Usermanagementsystem/model/User.java
+++ User_Management_System/src/main/java/com/example/Usermanagementsystem/model/User.java
@@ -1,12 +1,14 @@
-package com.example.Usermanagementsystem.model;
+package com.Usermanagementsystem.model;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -14,31 +16,31 @@ import java.time.LocalTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "users")
+@Table(name = "Users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "User_Id")
-    private Long UserId;
+    private Integer UserId;
 
-    @NotNull(message = "Username cannot be null")
-    @Size(min = 4, max = 30, message = "Username must be between 4 and 30 characters")
+    @NotNull(message = "Username can't be null")
+    @Size(min = 4, max = 30, message = "Username size must be between 4 and 30 characters")
     @Column(name = "User_Name", nullable = false, length = 30)
     private String username;
 
-    @NotNull(message = "Date of birth cannot be null")
-    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Date of birth should be in the format YYYY-MM-DD")
+    @NotNull(message = "Date of birth can't be null")
+    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Date of birth format YYYY-MM-DD")
     @Column(name = "date_of_birth", nullable = false)
     private String dateOfBirth;
 
-    @NotNull(message = "Email cannot be null")
+    @NotNull(message = "Email can't be null")
     @Email(message = "Invalid email format")
     @Column(name = "Email", nullable = false, unique = true)
     private String email;
 
-    @NotNull(message = "Phone number cannot be null")
-    @Pattern(regexp = "\\d{2}-\\d{10}", message = "Phone number should be in format xx-xxxxxxxxxx with first two digits as country code")
+    @NotNull(message = "Phone number can't be null")
+    @Pattern(regexp = "\\d{2}-\\d{10}", message = "Phone number format xx-xxxxxxxxxx with first two digits is country code")
     @Column(name = "Phone_Number", nullable = false, length = 12)
     private String phoneNumber;
 
