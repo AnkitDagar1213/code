f2ce4b08ea8842c828ca81a7056ee09bef1fe218
Dagarshab <112771048+Dagarshab@users.noreply.github.com>
2023-02-19 23:44:54 +0530

    Delete JpaConfig.java

--- User_Management_System/src/main/java/com/example/Usermanagementsystem/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.Usermanagementsystem;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@Configuration
-@EnableJpaRepositories(basePackages = "com.example.Usermanagementsystem.repository")
-public class JpaConfig {
-}
